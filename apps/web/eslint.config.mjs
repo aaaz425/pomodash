67728f5b93856ffc74482162f669fbe24c1d0e30
박유진 <aaaz425@naver.com>
2026-08-18 10:41:09 +0900

    chore: 웹/모바일 React Compiler 린트 진단 도구 추가, 웹 컴파일러 활성화

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,11 +1,16 @@
 import coreWebVitals from 'eslint-config-next/core-web-vitals';
 import typescript from 'eslint-config-next/typescript';
 import prettierConfig from 'eslint-config-prettier';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 const eslintConfig = [
   ...coreWebVitals,
   ...typescript,
   prettierConfig,
+  {
+    plugins: { 'react-compiler': reactCompiler },
+    rules: { 'react-compiler/react-compiler': 'error' },
+  },
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
